70729cabd1cbcbf5c9fc7225307ddc097274c09f
Frozen Breadstick <mattia.sydcanfam@gmail.com>
2025-10-16 12:19:26 +1100

    added second lidar, updated world file, added template files

--- launch/unomas_run.launch.py
+++ launch/unomas_run.launch.py
@@ -66,9 +66,9 @@ def generate_launch_description():
     # Start Gazebo to simulate the robot in the chosen world
     world_launch_arg = DeclareLaunchArgument(
         'world',
-        default_value='farm_environment_V2',
+        default_value='simple_trees',
         description='Which world to load',
-        choices=['farm_environment_V2']
+        choices=['simple_trees']
     )
     ld.add_action(world_launch_arg)
     gazebo = IncludeLaunchDescription(
